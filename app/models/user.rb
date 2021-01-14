02071417a606e5253d7c7a625b73402ff7bd4528
Marco Evair <mmacolluncoc@uni.pe>
2021-01-14 11:21:46 -0500

    validates tweet,user and likes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+  enum.role: {
+    member: 0,
+    admin: 1
+  }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  # email and password was create when we  was creating devise User
+  validates :name, presence: true
 end
