c4fdd5c1fc5bac17cc610e76d676ce2017be060c
Andrea Miranda <amiranda2510@gmail.com>
2021-01-14 15:17:56 -0500

    added views for devise, devise parameters on application controller and active storage for users avatars

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :avatar
   has_many :tweets, dependent: :destroy
   has_many :likes, dependent: :destroy
   devise :database_authenticatable, :registerable,
