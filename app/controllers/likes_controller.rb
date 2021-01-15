1e726e1501cbaed24e58b1170a606336ab507819
Andrea Miranda <amiranda2510@gmail.com>
2021-01-15 16:03:27 -0500

    added method destroy, find like in likes controller and render like button on tweets index view

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,14 +1,39 @@
 class LikesController < ApplicationController
   before_action :find_tweet
-  
+  before_action :find_like, only: [:destroy]
+
   def create
-    @tweet.likes.create(user_id: current_user.id)
-    redirect_to tweet_path(@tweet)
+    if already_liked
+      flash[:notice] = "You can't like more than once"
+    else
+      @tweet.likes.create(user_id: current_user.id)
+    end
+  
+    redirect_to tweets_path
+  end
+
+  def destroy
+    if !already_liked
+      flash[:notice] = 'Cannot unlike'
+    else
+      @like.destroy
+    end
+
+    redirect_to tweets_path
   end
 
   private
 
   def find_tweet
     @tweet = Tweet.find(params[:tweet_id])
+  end
+
+  def find_like
+    @like = @tweet.likes.find(params[:id])
+  end
+
+  def already_liked
+    Like.exists?(user_id: current_user.id, tweet_id:
+    params[:tweet_id])
   end
 end
