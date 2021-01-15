3cb4801978d11f1430931f825b2fe199f2da219a
Andrea Miranda <amiranda2510@gmail.com>
2021-01-15 01:57:55 -0500

    added  create to like controller and buttons and likes count to tweets index

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,14 @@
 class LikesController < ApplicationController
+  before_action :find_tweet
+  
+  def create
+    @tweet.likes.create(user_id: current_user.id)
+    redirect_to tweet_path(@tweet)
+  end
+
+  private
+
+  def find_tweet
+    @tweet = Tweet.find(params[:tweet_id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   #root 'home#index'
   root 'tweets#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :tweets
+  resources :tweets do
+    resources :likes
+  end
   devise_for :users
   
 end
