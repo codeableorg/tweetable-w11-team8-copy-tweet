7a7460de25507aa86b93004eaeeb5b5e6dab9cd6
Andrea Miranda <amiranda2510@gmail.com>
2021-01-14 23:09:19 -0500

    added show view for tweet, and added a form for replies. Also edit tweet controller in create and show

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -5,6 +5,9 @@ class TweetsController < ApplicationController
 
   def show
     @tweet = Tweet.find(params[:id])
+    @reply = Tweet.new
+    @reply.replied_to = @tweet
+    
   end
 
   def new
@@ -20,8 +23,12 @@ class TweetsController < ApplicationController
     @tweet = Tweet.new(tweet_params)
     @tweet.user = current_user
     @tweet.save
-
-    redirect_to tweets_path
+    if tweet_params[:replied_to_id]
+      redirect_to tweet_path(@tweet.replied_to)
+    else
+      redirect_to tweets_path
+    end
+    
   end
 
   def update
@@ -44,6 +51,6 @@ class TweetsController < ApplicationController
   private
 
   def tweet_params
-    params.require(:tweet).permit(:body, :user_id)
+    params.require(:tweet).permit(:body, :user_id, :replied_to_id)
   end
 end
--- app/models/tweet.rb
+++ app/models/tweet.rb
@@ -5,6 +5,7 @@ class Tweet < ApplicationRecord
                      foreign_key: 'replied_to_id',
                      dependent: :destroy,
                      inverse_of: false
-  belongs_to :replied_to, class_name: 'Tweet', optional: true, counter_cache: :replies_count
+  belongs_to :replied_to, class_name: 'Tweet', optional: true 
+  # counter_cache: :replies_count
   validates :body, presence: true, length: { maximum: 150 }
 end
